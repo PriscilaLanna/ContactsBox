d3701c475a63f7c8dbf56b85a5c5b688b04d77be
Priscila Lanna André <priscila.lanna@outlook.com>
2018-12-18 01:20:41 -0200

    Get, GetById, Delete

--- ContactsBox.Presentation.MVC/Controllers/HomeController.cs
+++ ContactsBox.Presentation.MVC/Controllers/HomeController.cs
@@ -3,6 +3,15 @@ using Microsoft.AspNetCore.Mvc;
 using ContactsBox.Presentation.MVC.Models;
 using AutoMapper;
 using ContactsBox.Domain.Entities;
+using ContactsBox.Presentation.MVC.ViewModels;
+using System;
+using System.Net.Http;
+using System.Collections;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace ContactsBox.Presentation.MVC.Controllers
 {
@@ -17,26 +26,75 @@ namespace ContactsBox.Presentation.MVC.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    HttpResponseMessage response = client.GetAsync("https://contactsboxapi.azurewebsites.net/api/contacts").Result;
+                    var lista = JsonConvert.DeserializeObject<IList<ContactViewModel>>(response.Content.ReadAsStringAsync().Result);
+                    return View(lista);
+                }
+            }
+
+            catch (Exception) { return View(new List<ContactViewModel>()); }
+
+
         }
-        
-        public IActionResult Contact(Contact contact)
+
+        public IActionResult Contact()
         {
-            return View();
+            return View(new ContactViewModel());
         }
-        
-        public IActionResult Save()
+
+        public IActionResult Edit(int id)
         {
+            ContactViewModel contact = new ContactViewModel();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    HttpResponseMessage response = client.GetAsync($"https://contactsboxapi.azurewebsites.net/api/contacts/{id}").Result;
+                    contact = JsonConvert.DeserializeObject<ContactViewModel>(response.Content.ReadAsStringAsync().Result);
+                }
+            }
+            catch (Exception) { }
+
+            return View(contact);
+        }
+
+        public IActionResult Save(ContactViewModel contact)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    if (contact.Id > 0)
+                    {
+                        HttpResponseMessage response = client.PutAsJsonAsync($"https://contactsboxapi.azurewebsites.net/api/contacts/", contact).Result;                     
+                    }
+                    else
+                    {
+                        HttpResponseMessage response = client.PostAsJsonAsync($"https://contactsboxapi.azurewebsites.net/api/contacts/", contact).Result;
+                    }
+                }
+            }
+            catch (Exception) { }
+
             return RedirectToAction("Index");
         }
 
-        public IActionResult Add()
+        public IActionResult Delete(int id)
         {
-            return RedirectToAction("Index");
-        }
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    HttpResponseMessage response = client.DeleteAsync($"https://contactsboxapi.azurewebsites.net/api/contacts/{id}").Result;
+                    var lista = JsonConvert.DeserializeObject<IList<ContactViewModel>>(response.Content.ReadAsStringAsync().Result);
+                }
+            }
+            catch (Exception) { }
 
-        public IActionResult Delete()
-        {
             return RedirectToAction("Index");
         }
 
